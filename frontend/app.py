6be7868ef7d33eb9f2bdd2abe75fa17b3dc5a683
Athar Rizwan <mathar.bese22seecs@seecs.edu.pk>
2024-12-25 20:48:34 +0500

    Working Chart

--- frontend/app.py
+++ frontend/app.py
@@ -17,9 +17,11 @@ def post_sensor_data():
     global sensor_data
     data = request.json  # Expect JSON data from the microcontroller
     if data:
-        sensor_data["heartbeat"] = data.get("heartbeat", 0)
-        sensor_data["oxygen"] = data.get("oxygen", 0)
-        sensor_data["gps"] = data.get("gps", {"latitude": 0.0, "longitude": 0.0})
+        sensor_data["heartbeat"] = data.get("bpm", 0)
+        sensor_data["oxygen"] = data.get("oxy", 0)
+        lat = data.get("lat", 0.0)  # Get 'lat' from the JSON
+        lon = data.get("lon", 0.0)  # Get 'lon' from the JSON
+        sensor_data["gps"] = {"latitude": lat, "longitude": lon}
         return jsonify({"status": "success", "message": "Data received successfully!"}), 200
     return jsonify({"status": "error", "message": "Invalid data"}), 400
 
